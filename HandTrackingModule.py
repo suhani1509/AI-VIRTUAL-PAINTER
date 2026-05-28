a8a6add19c920065eca74a32647b6884181d395d
Music Recommendation System <suhaniagghi09@gmail.com>
2026-05-28 16:16:37 +0530

    Uploading hand tracking module

---
+++ HandTrackingModule.py
@@ -0,0 +1,91 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+
+class handDetector():
+    def __init__(self,mode  = False,
+                 max_num_hands: int = 2,
+                 model_complexity: int = 1,
+                 min_detection_confidence: float = 0.5,
+                 min_tracking_confidence: float = 0.5) :
+        self.mode = mode
+        self.max_num_hands = max_num_hands
+        self.model_complexity = model_complexity
+        self.min_detection_confidence = min_detection_confidence
+        self.min_tracking_confidence = min_tracking_confidence
+
+        # default code
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode,self.max_num_hands,self.model_complexity, self.min_detection_confidence,self.min_tracking_confidence)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findhands(self , img , draw=True):
+         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+         self.results = self.hands.process(imgRGB)
+            # print(results.multi_hand_landmarks)
+         if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                 if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+         return img
+
+    def findposition(self, img, handNo=0, draw=True):
+        lmList = []
+
+        # PEHLE CHECK KARO: Kya MediaPipe ko koi haath mila?
+        if self.results.multi_hand_landmarks:
+
+            # Agar haath mila, tabhi loop chalega aur code crash nahi hoga
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 8, (255, 0, 255), cv2.FILLED)
+
+        return lmList
+
+
+
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = handDetector() #forming object of class
+
+
+
+    ctime = 0
+    ptime = 0
+
+    while (True):
+        success, img = cap.read()
+        img=detector.findhands(img)
+        lmList= detector.findposition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+
+
+        ctime = time.time()
+        fps = 1 / (ctime - ptime)
+        ptime = ctime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        cv2.imshow('Image', img)
+        cv2.waitKey(1)
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ virtualpaint.py
@@ -0,0 +1,10 @@
+# import cv2
+# import numpy as np
+# import time
+# import os
+# import HandTrackingModule as htm
+
+import mediapipe as mp
+
+print(mp)
+print(mp.__file__)
