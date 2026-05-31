3fa08dfafa0255ba51089609d0819315239fa96e
Music Recommendation System <suhaniagghi09@gmail.com>
2026-05-31 19:21:25 +0530

    adjusting brushthickness

--- virtualpaint.py
+++ virtualpaint.py
@@ -1,3 +1,4 @@
+import math
 from email import header
 
 import cv2
@@ -45,41 +46,36 @@ while True:
         # print (lmList)
 
         #position of tip of index and middle finger
-        x1, y1 = lmList[8][1:]
-        x2, y2 = lmList[12][1:]
-        x3, y3 = lmList[4][1:]
-        x4 , y4 =lmList[20][1:]
+        x1, y1 = lmList[8][1:]   #index
+        x2, y2 = lmList[12][1:]  #middle
+        x3, y3 = lmList[4][1:]  #thumb
+        x4 , y4 =lmList[20][1:]   #pinky
 
         #3 checking which finger is up
         fingers = detector.fingerUp()
         print(fingers)
 
 
-        #4 if selection mode 2 fingers are up
-        if fingers[0]==1 and fingers[1]==1 and fingers[2]==0 and fingers[3]==0:
-            brushthickness=brushthickness+1
-        if fingers[4]==1 and fingers[1]==0 and fingers[2]==0 and fingers[3]==0:
-            brushthickness = max(1, brushthickness - 1)
-            print(brushthickness)
 
-        if fingers == [1, 1, 1, 1, 1] and not saved:
+
+
+
+        #save image
+        if fingers == [0, 1, 1, 1, 0] and not saved:
             cv2.imwrite("Drawing.png", imgCanvas)
             print("Image Saved")
 
             saved = True
-            # if time.time() - saveMessageTime < 3:
-            #     cv2.putText(img, "Image Saved!", (400, 650),
-            #                 cv2.FONT_HERSHEY_SIMPLEX, 1,
-            #                 (0, 255, 0), 3)
 
-
-        elif fingers != [1, 1, 1, 1, 1]:
+        elif fingers != [0, 1, 1, 1, 0]:
             saved = False
 
+        #clean canvas
+        if fingers==[1,1,1,1,1]:
+            imgCanvas = np.zeros((720, 1280, 3), np.uint8)
+            print("Canvas Cleared!")
 
-
-
-
+        # 4 if selection mode 2 fingers are up
         if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0 and fingers[4] == 0:
             print("selection mode")
 
@@ -103,25 +99,54 @@ while True:
                     brushthickness=50
 
 
+        #5 if drawing 1 finger is up and dynamic thickness
+        if fingers == [1, 1, 0, 0, 0]:
+            distance = math.hypot(x1 - x3, y1 - y3)
 
+            # Jab distance 30 se kam ho (ungliyan chipki ho), tabhi thickness lock/unlock ho
+            if distance < 30:
+                thickness_locked = False  # Unlock ho gaya, ab distance badha kar size change karo
+                print("Thickness Unlocked - Adjusting...")
 
+            if not thickness_locked:
+                brushthickness = int(np.interp(distance, [30, 180], [5, 40]))
+                brushthickness = min(40, brushthickness)
 
+            # ---- DRAWING MODE (Only Index Up) ----
+            # Jaise hi sirf index up ho, thickness ko turant lock kar do
+        elif fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0:
+            thickness_locked = True  # Lock set kar diya taaki thumb fold hote waqt thickness na badle
+            print("Drawing Mode - Thickness Locked!")
 
+            if xp == 0 and yp == 0:
+                xp = x1
+                yp = y1
 
-
-
-        #5 if drawing 1 finger is up
-        if fingers[1]==1 and fingers[2]==0 and fingers[3]==0 and fingers[4]==0:
-            print("drawing mode")
-            if xp==0 and yp==0:
-                xp =x1
-                yp =y1
-            cv2.line(img,(xp,yp),(x1,y1),drawcolor,brushthickness)
+            cv2.line(img, (xp, yp), (x1, y1), drawcolor, brushthickness)
             cv2.line(imgCanvas, (xp, yp), (x1, y1), drawcolor, brushthickness)
-            xp =x1
-            yp =y1
+            xp = x1
+            yp = y1
+
+            # Agar koi aur gesture hai, toh drawing coordinates reset karein
+        else:
+            xp, yp = 0, 0
 
 
+
+
+
+
+    #next 2 lines me canvas me jaha draw kara hai wo 50 above hoga usse black kar dega and rest 50
+    # below usse background smjh kar white kar dega and usse imgInv me save kar dega
+    img_Gray = cv2.cvtColor(imgCanvas,cv2.COLOR_BGR2GRAY)
+    _ , imgInv =cv2.threshold(img_Gray,50,255,cv2.THRESH_BINARY_INV)
+
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+    # original image aur inverted mask ka and operation kar raha hai . kyuki maske me drwaing wala hissa black 0 tha
+    # toh original me image me thik use jagah canvas me jaha black hota and karke 0 kar dega and black bhole bana deta\
+    # hai and baaki hissa bilkul waisa he rahega
+    img = cv2.bitwise_and(img,imgInv)
+    img =cv2.bitwise_or(img,imgCanvas)
 
 
     #setting the header image in video ,,,frame set ho rahe hai img variable me and header img is saved in header
@@ -139,3 +164,4 @@ while True:
     cv2.imshow('img',img)
     cv2.imshow('Canvas', imgCanvas)
     cv2.waitKey(1)
+
